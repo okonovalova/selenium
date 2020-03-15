7fe688fccfea10c5ddd15eb00e07a05872d38626
olgazaynullina <zaynullina.olya@mail.ru>
2020-03-15 17:02:37 +0500

    checkCategoryTitleTest

--- src/teva_life/TevaLifeSeleniumTest.java
+++ src/teva_life/TevaLifeSeleniumTest.java
@@ -1,34 +1,20 @@
 package teva_life;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import teva_life.pages.CategoryPage;
+import teva_life.pages.DetailsStoryPage;
 
 import java.time.Duration;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TevaLifeSeleniumTest {
-
-    private boolean clickLoadMoreBtn(List <WebElement> categoryBodyChilds, WebDriver driver ) throws InterruptedException {
-        for (WebElement webElement:categoryBodyChilds){
-            String className = webElement.getAttribute("class");
-            if (className != null) {
-                if (className.equals("category__load-more button button--blue")) {
-                    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(false);", webElement);
-                    webElement.click();
-                    Thread.sleep(2000);
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
     @Test
     public void checkCountStories() throws InterruptedException {
 
@@ -45,5 +31,28 @@ class TevaLifeSeleniumTest {
         int countStoriesFromTitle = categoryPage.getCountStoriesFromTitle();
         int actualCountStories = categoryPage.getActualCountStories();
         assertEquals(countStoriesFromTitle, actualCountStories);
+    }
+
+    @Test
+    public void CheckCategoryTitleTest() {
+        FirefoxDriver driver = new FirefoxDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        CategoryPage categoryPage = new CategoryPage(driver, wait);
+        categoryPage.maximizeWindow();
+        categoryPage.openPage();
+        categoryPage.clickRegionOptionControl("en-EU");
+        categoryPage.clickBtnSubmitRegionOptionControl();
+        categoryPage.clickBtnAllowCookies();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("category__panel")));
+        WebElement categoryPanel = driver.findElement(By.className("category__panel"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", categoryPanel);
+        WebElement firstStoryCard = driver.findElement(By.className("card__title title title--xs title--green-emerald"));
+        String firstStoryCardTitle = firstStoryCard.getText();
+        firstStoryCard.click();
+
+        DetailsStoryPage detailsStoryPage = new DetailsStoryPage(driver, wait);
+        String actualStoryTitle = detailsStoryPage.getHeaderStory();
+        assertEquals(firstStoryCardTitle, actualStoryTitle);
+
     }
 }
---
+++ src/teva_life/pages/DetailsStoryPage.java
@@ -0,0 +1,29 @@
+package teva_life.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DetailsStoryPage {
+    private RemoteWebDriver driver;
+    private WebDriverWait wait;
+
+    public DetailsStoryPage(RemoteWebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    private void waitByAppearHeaderStory() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("article__header")));
+    }
+
+    public String getHeaderStory() {
+        waitByAppearHeaderStory();
+        WebElement arcticleHeaderElement = driver.findElement(By.className("article__header"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", arcticleHeaderElement);
+        return driver.findElement(By.className("title title--xl title--green-emerald indexed-content")).getText();
+    }
+}
